f0d698737e62333acaed419272bddf550f802918
Byron Austin <Byron.L.Austin@gmail.com>
2013-08-12 10:20:08 -0600

    Create SMART.h

---
+++ SMART.h
@@ -0,0 +1,21 @@
+//---------------------------------------------------------------------------
+
+#ifndef SMARTH
+#define SMARTH
+//---------------------------------------------------------------------------
+#include <Classes.hpp>
+#include <Controls.hpp>
+#include <StdCtrls.hpp>
+#include <Forms.hpp>
+//---------------------------------------------------------------------------
+class TSMARTInfo : public TForm
+{
+__published:  // IDE-managed Components
+private:	// User declarations
+public:		// User declarations
+        __fastcall TSMARTInfo(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TSMARTInfo *SMARTInfo;
+//---------------------------------------------------------------------------
+#endif
