cd82d7c83c69ced6c23750f43f0ecdbeddcd39dc
Byron Austin <Byron.L.Austin@gmail.com>
2013-08-12 10:13:04 -0600

    Create Language.cpp

---
+++ Language.cpp
@@ -0,0 +1,18 @@
+//---------------------------------------------------------------------------
+
+#include <vcl.h>
+#pragma hdrstop
+
+#include "Language.h"
+#include "TabPages.h"
+//---------------------------------------------------------------------------
+#pragma package(smart_init)
+#pragma resource "*.dfm"
+TLanguageForm *LanguageForm;
+//---------------------------------------------------------------------------
+__fastcall TLanguageForm::TLanguageForm(TComponent* Owner)
+        : TForm(Owner)
+{
+}
+//---------------------------------------------------------------------------
+
